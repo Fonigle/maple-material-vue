a2531ecad4e9c6b5c6a100f24c6a0a99c684971f
huangyu <fonigle@163.com>
2019-05-07 10:22:11 +0800

    add files for dev.

---
+++ dev/app.vue
@@ -0,0 +1,28 @@
+<template>
+    <div class="app">
+        <mdc-button @click="handleButtonClick">确定</mdc-button>
+        <mdc-icon-button-toggle v-model="test">
+            <i class="mdc-icon-button__icon mdc-icon-button__icon--on mpi-heart"></i>
+            <i class="mdc-icon-button__icon mpi-heart-outline"></i>
+        </mdc-icon-button-toggle>
+        <button @click="test=!test">try</button>
+
+    </div>
+</template>
+<script lang="tsx">
+    import { Vue, Component, Prop, Mixins, Watch } from "vue-property-decorator";
+
+    @Component
+    export default class MyComponent extends Vue {
+        test = true;
+
+        handleButtonClick() {
+            console.log(1);
+        }
+    }
+</script>
+<style lang="scss">
+    body {
+        background: #fff !important;
+    }
+</style>
---
+++ dev/index.ts
@@ -0,0 +1,13 @@
+import Vue from 'vue';
+
+import App from './app.vue';
+
+import MapleMaterialVue from '@/index';
+Vue.use(MapleMaterialVue);
+
+import '../_intermediate/iconfonts/maple-icon.scss';
+
+const vm = new Vue({
+    el: '#app',
+    render: h => h(App),
+});
