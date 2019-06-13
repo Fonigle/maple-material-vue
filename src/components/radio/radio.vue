b003374f41793e2ce5333d093bfd8911dd175f4c
huangyu <fonigle@163.com>
2019-06-13 18:17:46 +0800

    add comments.

--- src/components/radio/radio.vue
+++ src/components/radio/radio.vue
@@ -12,89 +12,96 @@
     </div>
 </template>
 <script lang="ts">
-    import { Vue, Component, Mixins, Prop } from "vue-property-decorator";
-    import twowayFactory from "mixins/two-way";
-    import { MDCRadio } from "@material/radio";
-    import { MDCFormField } from '@material/form-field';
-    import IdMixin from "mixins/id";
+import { Vue, Component, Mixins, Prop } from "vue-property-decorator";
+import twowayFactory from "mixins/two-way";
+import { MDCRadio } from "@material/radio";
+import { MDCFormField } from '@material/form-field';
+import IdMixin from "mixins/id";
 
-    import RadioCore from './radio-core.vue';
+import RadioCore from './radio-core.vue';
+
+/**
+ * component <mdc-radio>
+ *
+ * <mdc-radio>组件
+ * @export
+ * @class MdcRadio
+ * @extends {Mixins(twowayFactory(), IdMixin)}
+ */
+@Component({
+    components: { RadioCore }
+})
+export default class MdcRadio extends Mixins(twowayFactory(), IdMixin) {
+    /**
+     * disabled mark
+     *
+     * 禁用状态标记
+     * @type {boolean}
+     * @memberof MdcRadio
+     */
+    @Prop({
+        type: Boolean,
+        default: false
+    })
+    disabled!: boolean;
 
     /**
-     * component <mdc-radio>
+     * name of the native input control
      *
-     * <mdc-radio>组件
-     * @export
-     * @class MdcRadio
-     * @extends {Mixins(twowayFactory(), IdMixin)}
+     * 原生input组件的name
+     * @type {string}
+     * @memberof MdcRadio
      */
-    @Component({
-        components: { RadioCore }
+    @Prop({
+        type: String
     })
-    export default class MdcRadio extends Mixins(twowayFactory(), IdMixin) {
-        /**
-         * disabled mark
-         *
-         * 禁用状态标记
-         * @type {boolean}
-         * @memberof MdcRadio
-         */
-        @Prop({
-            type: Boolean,
-            default: false
-        })
-        disabled!: boolean;
+    name?: string
 
-        /**
-         * name of the native input control
-         *
-         * 原生input组件的name
-         * @type {string}
-         * @memberof MdcRadio
-         */
-        @Prop({
-            type: String
-        })
-        name?: string
+    /**
+     * value of native input control
+     *
+     * 原生input组件的绑定值
+     * @type {*}
+     * @memberof MdcRadio
+     */
+    @Prop()
+    value?: any
 
-        /**
-         * value of native input control
-         *
-         * 原生input组件的绑定值
-         * @type {*}
-         * @memberof MdcRadio
-         */
-        @Prop()
-        value?: any
+    /**
+     * content of the label
+     *
+     * 标签内容
+     * @type {string}
+     * @memberof MdcRadio
+     */
+    @Prop({
+        type: String
+    })
+    label?: string
 
-        /**
-         * content of the label
-         *
-         * 标签内容
-         * @type {string}
-         * @memberof MdcRadio
-         */
-        @Prop({
-            type: String
-        })
-        label?: string
-
-        /**
-         * html element of form-filed
-         *
-         * 'form-filed' HTML元素
-         * @readonly
-         * @memberof MdcRadio
-         */
-        get eleFormField() {
-            return this.$refs['form-field'] as Element;
-        }
-
-        handleCoreMounted(radio: MDCRadio) {
-            const formField = new MDCFormField(this.eleFormField);
-            formField.input = radio;
-        }
+    /**
+     * html element of form-filed
+     *
+     * 'form-filed' HTML元素
+     * @readonly
+     * @memberof MdcRadio
+     */
+    get eleFormField() {
+        return this.$refs['form-field'] as Element;
     }
+
+    /**
+     * handler for 'radio-core-mounted' event
+     *
+     * 'radio-core-mounted' 事件处理
+     * @param {MDCRadio} radio
+     * @memberof MdcRadio
+     */
+    handleCoreMounted(radio: MDCRadio) {
+        const formField = new MDCFormField(this.eleFormField);
+        formField.input = radio;
+    }
+}
 </script>
 <style lang="scss">
     @import "@material/form-field/mdc-form-field";
